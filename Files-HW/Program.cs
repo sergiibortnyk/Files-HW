3f1ce41a6a5c4d968c64a5a67a471b94621d2cb3
Sergii Bortnyk <sbortnyk@gmail.com>
2016-04-27 15:29:00 +0300

    files homework project

---
+++ Files-HW/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files_HW
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> inputList = ReadFile("input.txt");
+            HashSet<string> whiteList = ReadFile("whitelist.txt");
+            HashSet<string> blackList = ReadFile("blacklist.txt");           
+           
+            var outputWhiteList = inputList.Intersect(whiteList);
+            var outputBlackList = inputList.Except(blackList);
+            var outputBlackWhiteList = outputWhiteList.Except(blackList);
+            File.WriteAllLines("output_whitelist.txt", outputWhiteList);
+            File.WriteAllLines("output_blacklist.txt", outputBlackList);
+            File.WriteAllLines("output_black_and_white_list.txt", outputBlackWhiteList);                                                
+        }
+
+        public static HashSet<string> ReadFile (string fileName)
+        {
+            HashSet<string> readToList = new HashSet<string>();
+            using (StreamReader ReaderFile = new StreamReader(fileName))
+            {
+                string listLine;
+                while ((listLine = ReaderFile.ReadLine()) != null)
+                {
+                    readToList.Add(listLine);
+                }
+            }
+            return readToList;
+        }
+    }
+}
